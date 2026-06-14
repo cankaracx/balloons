0fa1f211c8b98077eac4c9ed6acf237c46201099
Can KARACA <cankaracx@gmail.com>
2026-06-14 21:45:12 +0300

    Complete Frontend Redesign

--- src/components/public/Site.tsx
+++ src/components/public/Site.tsx
@@ -4,12 +4,14 @@ import { LangProvider } from "./LangProvider";
 import Nav from "./Nav";
 import Hero from "./Hero";
 import About from "./About";
-import Sponsors from "./Sponsors";
+import SponsorWall from "./SponsorWall";
 import Team from "./Team";
 import Gallery from "./Gallery";
 import Outreach from "./Outreach";
+import Partner from "./Partner";
 import Contact from "./Contact";
 import Footer from "./Footer";
+import ScrollLine from "./ScrollLine";
 import type { Locale, SiteData } from "@/lib/types";
 
 export default function Site({ data, initialLang }: { data: SiteData; initialLang: Locale }) {
@@ -18,13 +20,15 @@ export default function Site({ data, initialLang }: { data: SiteData; initialLan
   return (
     <LangProvider initial={initialLang}>
       <Nav brand={content.brand} />
-      <main>
+      <main className="relative">
+        <ScrollLine />
         <Hero hero={content.hero} brand={content.brand} />
         <About about={content.about} />
-        <Sponsors sponsors={content.sponsors} list={sponsors} />
+        <SponsorWall sponsors={content.sponsors} list={sponsors} />
         <Team team={content.team} list={members} />
         <Gallery gallery={content.gallery} list={gallery} />
         <Outreach outreach={content.outreach} />
+        <Partner sponsors={content.sponsors} />
         <Contact contact={content.contact} />
       </main>
       <Footer brand={content.brand} />
